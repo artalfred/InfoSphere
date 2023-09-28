3bff560ccfcd2b041006c340ac3825db8b2c2bfc
ArtAlfred <bernalesartalfred@gmail.com>
2023-09-28 21:46:17 +0800

    Fixed Error handling for Images & Data

--- src/components/resusable/Content.jsx
+++ src/components/resusable/Content.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-
-export default function Cards({ img, title, abstract, byline }) {
+function Content({ img, title, abstract, byline }) {
   return (
     <div className="card">
-      <img src={img} className="img-fluid img_size_sm" alt="..." />
+      <img src={img} className="img-fluid " alt="..." />
       <div className="card-body mt-3">
-        <h6 className="fw-semibold body-text">{title}</h6>
+        <h6 className="fw-bold body-text">{title}</h6>
         <p className="text-secondary body-text lh-base p-0">{abstract}</p>
         <p className="text-muted mt-2" style={{ fontSize: "12px" }}>
           {byline}
@@ -14,3 +12,5 @@ export default function Cards({ img, title, abstract, byline }) {
     </div>
   );
 }
+
+export default Content;
--- src/components/resusable/Pages.jsx
+++ src/components/resusable/Pages.jsx
@@ -3,10 +3,10 @@ import Cards from "./Cards";
 import { useEffect } from "react";
 import OtherNews from "./OtherNews";
 import { useCategory } from "../Functionality/CategoryContext";
-import Error from "./Error";
 import { useApi } from "../Functionality/ApiContext";
 
 import err from "../../img/err.png";
+import Content from "./Content";
 
 function Pages({ API }) {
   const { category, setCategory } = useCategory();
@@ -35,99 +35,48 @@ function Pages({ API }) {
                   {API?.slice(0, 2).map((item, index) => (
                     <div key={index}>
                       {!item.title ? (
-                        <div className="border-bottom pb-4">
-                          <Error message="Error Fetching Data" />
+                        <div className="card pb-4">
+                          <img src={err} className="img-fluid " alt="..." />
                         </div>
                       ) : (
                         <Link
                           reloadDocument
                           to={`/${category}/${item.id}/${
                             item.multimedia[0]?.caption?.replace(/\s+/g, "-") ||
-                            "Undefined"
+                            item.title?.replace(/\s+/g, "-")
                           }`}
                         >
-                          <div className="card">
-                            <div className="card-body border-bottom mb-3">
-                              <h6 className="fw-semibold body-text">
-                                {item.title}
-                              </h6>
-                              <p className="text-secondary body-text lh-base p-0">
-                                {item.abstract}
-                              </p>
-                              <p
-                                className="text-muted mt-2"
-                                style={{ fontSize: "12px" }}
-                              >
-                                {item.byline}
-                              </p>
-                            </div>
-                          </div>
-                        </Link>
-                      )}
-                    </div>
-                  ))}
-                  {API?.slice(2, 3).map((item, index) => (
-                    <div key={index}>
-                      {!item.title ? (
-                        <div className="border-bottom pb-4">
-                          <Error message="Error Fetching Data" />
-                        </div>
-                      ) : (
-                        <Link
-                          reloadDocument
-                          to={`/${category}/${
-                            item.id
-                          }/${item.multimedia[0]?.caption?.replace(
-                            /\s+/g,
-                            "-"
-                          )}`}
-                        >
-                          <div className="card">
-                            <div className="card-body">
-                              <h6 className="fw-semibold body-text">
-                                {item.title}
-                              </h6>
-                              <p className="text-secondary body-text lh-base p-0">
-                                {item.abstract}
-                              </p>
-                              <p
-                                className="text-muted mt-2"
-                                style={{ fontSize: "12px" }}
-                              >
-                                {item.byline}
-                              </p>
-                            </div>
-                          </div>
+                          <Content
+                            img={item.multimedia?.[0]?.url}
+                            title={item.title}
+                            abstract={item.abstract}
+                            byline={item.byline}
+                          />
                         </Link>
                       )}
                     </div>
                   ))}
                 </div>
 
+                {/* LARGE IMAGE */}
                 <div className="col-12 col-md-8 col-lg-8 order-1 order-sm-1 order-md-1 order-lg-2">
                   {API?.slice(3, 4).map((item, index) => (
                     <div key={index}>
                       {!item.title ? (
-                        <div className="card">
-                          <img
-                            src={item.multimedia?.[0]?.url && err}
-                            className="img-fluid "
-                            alt="..."
-                          />
+                        <div className="card pb-4">
+                          <img src={err} className="img-fluid " alt="..." />
                         </div>
                       ) : (
                         <Link
                           reloadDocument
-                          to={`/${category}/${
-                            item.id
-                          }/${item.multimedia[0]?.caption?.replace(
-                            /\s+/g,
-                            "-"
-                          )}`}
+                          to={`/${category}/${item.id}/${
+                            item.multimedia[0]?.caption?.replace(/\s+/g, "-") ||
+                            item.title?.replace(/\s+/g, "-")
+                          }`}
                         >
                           <div className="card">
                             <img
-                              src={item.multimedia?.[0]?.url || "Hello"}
+                              src={item.multimedia?.[0]?.url}
                               className="img-fluid "
                               alt="..."
                             />
@@ -157,106 +106,29 @@ function Pages({ API }) {
             {/* SECTION 2 */}
             <div className="border-bottom d-grid gap-4 py-3">
               <div className="row border-bottom">
-                <div className="col-12 col-md-4 col-lg-4">
-                  {API?.slice(4, 7).map((item, index) => (
-                    <div key={index}>
-                      {!item.title ? (
-                        <div className="border-bottom pb-4">
-                          <Error message="Error Fetching Data" />
-                        </div>
-                      ) : (
-                        <Link
-                          reloadDocument
-                          to={`/${category}/${
-                            item.id
-                          }/${item.multimedia[0]?.caption?.replace(
-                            /\s+/g,
-                            "-"
-                          )}`}
-                        >
-                          <div className="card ">
-                            <div className="card-body border-bottom mt-3">
-                              <h6 className="fw-semibold body-text">
-                                {item.title}
-                              </h6>
-                              <p className="text-secondary body-text lh-base p-0">
-                                {item.abstract}
-                              </p>
-                              <p
-                                className="text-muted mt-2"
-                                style={{ fontSize: "12px" }}
-                              >
-                                {item.byline}
-                              </p>
-                            </div>
-                          </div>
-                        </Link>
-                      )}
-                    </div>
-                  ))}
-                  {API?.slice(7, 8).map((item, index) => (
-                    <div key={index}>
-                      {!item ? (
-                        <Error message="Error Fetching Data" />
-                      ) : (
-                        <Link
-                          reloadDocument
-                          to={`/${category}/${
-                            item.id
-                          }/${item.multimedia[0]?.caption?.replace(
-                            /\s+/g,
-                            "-"
-                          )}`}
-                        >
-                          <div className="card">
-                            <div className="card-body mt-3">
-                              <h6 className="fw-semibold body-text">
-                                {item.title}
-                              </h6>
-                              <p className="text-secondary body-text lh-base p-0">
-                                {item.abstract}
-                              </p>
-                              <p
-                                className="text-muted mt-2"
-                                style={{ fontSize: "12px" }}
-                              >
-                                {item.byline}
-                              </p>
-                            </div>
-                          </div>
-                        </Link>
-                      )}
-                    </div>
-                  ))}
-                </div>
-                <div className="col-md-8 col-lg-8">
+                {/* LARGE IMAGE */}
+                <div className="col">
                   {API?.slice(8, 9).map((item, index) => (
                     <div key={index}>
                       {!item.title ? (
-                        <div className="card">
-                          <img
-                            src={item.multimedia?.[0]?.url && err}
-                            className="img-fluid "
-                            alt="..."
-                          />
+                        <div className="card pb-4">
+                          <img src={err} className="img-fluid" alt="..." />
                         </div>
                       ) : (
                         <Link
                           reloadDocument
-                          to={`/${category}/${
-                            item.id
-                          }/${item.multimedia[0]?.caption?.replace(
-                            /\s+/g,
-                            "-"
-                          )}`}
+                          to={`/${category}/${item.id}/${
+                            item.multimedia[0]?.caption?.replace(/\s+/g, "-") ||
+                            item.title?.replace(/\s+/g, "-")
+                          }`}
                         >
                           <div className="card">
                             <div
                               className="news_img_adjust overflow-hidden"
-                              style={{ maxHeight: "26rem" }}
+                              style={{ maxHeight: "35rem" }}
                             >
                               <img
-                                src={item.multimedia?.[0]?.url || "Hello"}
+                                src={item.multimedia?.[0]?.url}
                                 className="img-fluid"
                                 alt="..."
                               />
@@ -283,30 +155,35 @@ function Pages({ API }) {
                 </div>
               </div>
 
-              {/* SECTION 3 */}
               <div className="row gy-4">
-                {API?.slice(9, 14).map((item, index) => {
+                {API?.slice(9, 15).map((item, index) => {
                   return (
                     <div className="col-12 col-md-4" key={index}>
                       {!item.title ? (
-                        <Error message="Error Fetching Data" />
+                        <div className="card pb-4">
+                          <img src={err} className="img-fluid " alt="ERROR" />
+                        </div>
                       ) : (
-                        <Cards
-                          category={category}
-                          id={item.id}
-                          item={
-                            item.multimedia?.[0].caption?.replace(
+                        <Link
+                          reloadDocument
+                          to={`/${category}/${item.id}/${
+                            item.multimedia[0]?.caption?.replace(/\s+/g, "-") ||
+                            item.title?.replace(/\s+/g, "-")
+                          }`}
+                        >
+                          <Cards
+                            category={category}
+                            id={item.id}
+                            item={item.multimedia?.[0].caption?.replace(
                               /\s+/g,
                               "-"
-                            ) || "Content Not Available"
-                          }
-                          img={
-                            item.multimedia?.[0]?.url || "Content Not Available"
-                          }
-                          title={item.title}
-                          abstract={item.abstract}
-                          byline={item.byline}
-                        />
+                            )}
+                            img={item.multimedia?.[0]?.url}
+                            title={item.title}
+                            abstract={item.abstract}
+                            byline={item.byline}
+                          />
+                        </Link>
                       )}
                     </div>
                   );
@@ -317,95 +194,51 @@ function Pages({ API }) {
         </div>
 
         {/* SIDE NEWS */}
-        <div className="col-xs-12 col-md-12 col-lg-3 border-start order-2 order-md-2 order-lg-2">
+        <div className="col-xs-12 col-md-12 col-lg-3 border-start border-bottom order-2 order-md-2 order-lg-2">
           {API?.slice(15, 16).map((item, index) => (
             <div key={index}>
               {!item.title ? (
-                <Error message="Error Fetching Data" />
+                <div className="card pb-4">
+                  <img src={err} className="img-fluid " alt={item.title} />
+                </div>
               ) : (
                 <Link
                   reloadDocument
-                  to={`/${category}/${
-                    item.id
-                  }/${item.multimedia[0]?.caption?.replace(/\s+/g, "-")}`}
+                  to={`/${category}/${item.id}/${
+                    item.multimedia[0]?.caption?.replace(/\s+/g, "-") ||
+                    item.title?.replace(/\s+/g, "-")
+                  }`}
                 >
-                  <div className="card ">
-                    <img
-                      src={item.multimedia?.[0]?.url || "Hello"}
-                      className="img-fluid"
-                      alt="..."
-                    />
-                    <div className="card-body p-0 border-bottom mt-3">
-                      <h6 className="fw-semibold body-text">{item.title}</h6>
-                      <p className="text-secondary body-text lh-base p-0">
-                        {item.abstract}
-                      </p>
-                      <p
-                        className="text-muted mt-2"
-                        style={{ fontSize: "12px" }}
-                      >
-                        {item.byline}
-                      </p>
-                    </div>
-                  </div>
+                  <Content
+                    img={item.multimedia?.[0]?.url}
+                    title={item.title}
+                    abstract={item.abstract}
+                    byline={item.byline}
+                  />
                 </Link>
               )}
             </div>
           ))}
-          {API?.slice(16, 24).map((item, index) => (
+          {API?.slice(16, 21).map((item, index) => (
             <div key={index}>
               {!item.title ? (
-                <Error message="Error Fetching Data" />
+                <div className="card pb-4">
+                  <img src={err} className="img-fluid " alt="..." />
+                </div>
               ) : (
                 <Link
                   reloadDocument
-                  to={`/${category}/${
-                    item.id
-                  }/${item.multimedia[0]?.caption?.replace(/\s+/g, "-")}`}
+                  to={`/${category}/${item.id}/${
+                    item.multimedia[0]?.caption?.replace(/\s+/g, "-") ||
+                    item.title?.replace(/\s+/g, "-")
+                  }`}
                 >
-                  <div className="card ">
-                    <div className="card-body p-0 border-bottom mt-3">
-                      <h6 className="fw-semibold body-text">{item.title}</h6>
-                      <p className="text-secondary body-text lh-base p-0">
-                        {item.abstract}
-                      </p>
-                      <p
-                        className="text-muted mt-2"
-                        style={{ fontSize: "12px" }}
-                      >
-                        {item.byline}
-                      </p>
-                    </div>
-                  </div>
-                </Link>
-              )}
-            </div>
-          ))}
-          {API?.slice(25, 26).map((item, index) => (
-            <div key={index}>
-              {!item.title ? (
-                <Error message="Error Fetching Data" />
-              ) : (
-                <Link
-                  reloadDocument
-                  to={`/${category}/${
-                    item.id
-                  }/${item.multimedia[0]?.caption?.replace(/\s+/g, "-")}`}
-                >
-                  <div className="card ">
-                    <div className="card-body p-0 mt-3">
-                      <h6 className="fw-semibold body-text">{item.title}</h6>
-                      <p className="text-secondary body-text lh-base p-0">
-                        {item.abstract}
-                      </p>
-                      <p
-                        className="text-muted mt-2"
-                        style={{ fontSize: "12px" }}
-                      >
-                        {item.byline}
-                      </p>
-                    </div>
-                  </div>
+                  <Content
+                    img={item.multimedia?.[0]?.url}
+                    title={item.title}
+                    abstract={item.abstract}
+                    byline={item.byline}
+                  />
                 </Link>
               )}
             </div>
